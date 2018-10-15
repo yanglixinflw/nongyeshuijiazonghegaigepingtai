426fb57fa947b8e4193b49244bc65bc858fb73be
cph-code <cheng19970210@qq.com>
2018-10-15 15:24:04 +0800

    loginMock

--- mock/login.js
+++ mock/login.js
@@ -1,17 +1,17 @@
 export const LoginHandler = (req, res) => {
     const { accountName, passWord, CAPTCHA } = req.body
-    if (accountName === 'admin' && passWord === '123456' && CAPTCHA) {
-        res.json({
+    if(accountName==='admin'&&passWord==="123456" && CAPTCHA){
+        res.send({
             status: '登录成功',
-            code: 0
+            massage: '登录成功',
+            code: 0,
         })
-    }else {
-        res.json({
-            status: '登录失败',
-            massage: '密码错误',
-            code: -1
-        })
+        return;
     }
-    
+    res.send({
+        status: '登录失败',
+        massage: '密码错误',
+        code: -1
+    })
 }
 export default LoginHandler
