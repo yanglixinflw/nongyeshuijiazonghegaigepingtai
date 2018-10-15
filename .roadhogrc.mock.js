9a56f36647b4c4b79d610f2fd5ea1b4f9d50be70
cph-code <cheng19970210@qq.com>
2018-10-15 14:55:01 +0800

    mock

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,4 +1,9 @@
+import {  delay } from 'roadhog-api-doc';
+import LoginHandler from './mock/login'
 
-export default {
-    
-};
+// 是否禁用代理
+const noProxy = process.env.NO_PROXY === 'true';
+const proxy ={
+    'POST /api/login':LoginHandler
+}
+export default (noProxy ? {} : delay(proxy, 1000));
