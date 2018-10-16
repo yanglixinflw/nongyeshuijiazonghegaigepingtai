3721882ad504d22e8029efe440814f7099debce7
cph-code <cheng19970210@qq.com>
2018-10-16 09:03:04 +0800

    login

--- src/models/Moisture.js
+++ src/models/Moisture.js
@@ -6,10 +6,9 @@ export default {
         *fetch({ payload }, { call, put }) {  // eslint-disable-line
           const data = yield call(queryMoisture, payload)
           yield put({ type: 'fetchOk', payload: data })
-          console.log('connect成功')
+          // console.log('connect成功')
         }
       },
-    
       reducers: {
         fetchOk (state, { payload }) {
             // console.log(state)
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import request from '../utils/request';
 export function queryMoisture(){
-  return request('/api/data/moisture');
+  return request('/api/data/moisture',{
+    method:'POST',
+  });
 }
-export function queryLogin(){
-  return request('/api/login');
+export function queryLogin(params){
+  return request('/api/login',{
+    method:'POST',
+    body:params
+  });
 }
