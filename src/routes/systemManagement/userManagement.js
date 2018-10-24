9b9b632cdc5f29b1ce1eb791398c735ca798b803
zxj462602 <z14758078652@163.com>
2018-10-24 15:40:41 +0800

    userMgr

--- src/routes/systemManagement/userManagement.js
+++ src/routes/systemManagement/userManagement.js
@@ -2,36 +2,36 @@ import React from "react";
 import UserManagement from '../../components/systemManagement/userManagement';
 import { connect } from 'dva';
 import {Spin} from 'antd'
-// @connect(({ userManagement, loading }) => ({
-//     userManagement,
-//     loading: loading.models.userManagement
-// }))
+@connect(({ userManagement, loading }) => ({
+    userManagement,
+    loading: loading.models.userManagement
+}))
 export default class extends React.Component{
-    // componentDidMount() {
-    //     const { dispatch } = this.props;
-    //     dispatch({
-    //         type: 'userManagement/fetch',
-    //         payload:{
-    //             "name": "",
-    //             "mobile": "",
-    //             "roleId": 0,
-    //             "pageIndex": 0,
-    //             "pageSize": 10
-    //           }
-    //     });//type来选择请求的接口，payload为传给后台的参数
-    // }
+    componentDidMount() {
+        const { dispatch } = this.props;
+        dispatch({
+            type: 'userManagement/fetch',
+            payload:{
+                "name": "",
+                "mobile": "",
+                "roleId": 0,
+                "pageIndex": 0,
+                "pageSize": 10
+              }
+        });//type来选择请求的接口，payload为传给后台的参数
+    }
     render(){
-        // let { userManagement, loading } = this.props;
-        // // console.log(userManagement)
-        // let arr = Object.keys(userManagement);
-        // if (arr.length ===0 ) return userManagement = null;
+        let { userManagement, loading } = this.props;
+        let arr = Object.keys(userManagement);
+        if (arr.length ===0 ) return userManagement = null;
+        // console.log(userManagement)
         return (
             <div>
-                {/* <Spin size='large' spinning={loading}> */}
+                <Spin size='large' spinning={loading}>
                     <UserManagement
-                        // {...this.props}
+                        {...this.props}
                     />
-                {/* </Spin> */}
+                </Spin>
             </div>
         )
     }
