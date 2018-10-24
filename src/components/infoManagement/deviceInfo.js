241b6be88ab0b3c3f397730bfe00b628120ebb37
cph-code <cheng19970210@qq.com>
2018-10-24 13:19:45 +0800

    setSHOW

--- src/components/infoManagement/deviceInfo.js
+++ src/components/infoManagement/deviceInfo.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import styles from './index.less'
-import { Form, 
-    Button, 
-    Input, 
-    Select, 
-    Cascader, 
-    Table ,
+import {
+    Form,
+    Button,
+    Input,
+    Select,
+    Cascader,
+    Table,
     Modal,
-    Checkbox} from 'antd'
+    Checkbox,
+    Row,
+    Col
+} from 'antd'
 const Item = Form.Item
 const Option = Select.Option
 
@@ -17,7 +21,7 @@ let postOption = {
     credentials: "include",
     mode: 'cors',
     headers: new Headers({
-      'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
     }),
 }
 
@@ -57,6 +61,21 @@ const dataIndex = [
     'warningRules',
     'lastRequestTime'
 ]
+// 源columns拥有编号
+const sourceColumns = [
+    { title: "设备ID", dataIndex: "deviceId" ,number:0},
+    { title: "设备型号", dataIndex: "deviceTypeName" ,number:1},
+    { title: "设备名称", dataIndex: "name" ,number:2},
+    { title: "设备安装地", dataIndex: "installAddr" ,number:3},
+    { title: "地理坐标", dataIndex: "IP" ,number:4},
+    { title: "启用日期", dataIndex: "enableTime",number:5 },
+    { title: "运维公司", dataIndex: "opsCompony",number:6 },
+    { title: "管护人员", dataIndex: "managerName",number:7 },
+    { title: "网关地址", dataIndex: "gatewayAddr" ,number:8 },
+    { title: "出厂编号", dataIndex: "factoryNumber" ,number:9 },
+    { title: "预警规则", dataIndex: "warningRules",number:10  },
+    { title: "更新时间", dataIndex: "lastRequestTime" ,number:11 }
+]
 export default class extends Component {
     constructor(props) {
         super(props)
@@ -64,6 +83,7 @@ export default class extends Component {
         this.state = {
             // 显示设置可见
             showSetVisible: false,
+            // showSetVisible: true,
             // 表头
             columns: [],
             // 数据总数
@@ -73,7 +93,7 @@ export default class extends Component {
             // 表格数据源
             data: props.data.data.items,
             // 表格数据
-            tableData:[],
+            tableData: [],
             // 翻页携带查询条件
         }
         // console.log(this.state.data)
@@ -92,7 +112,7 @@ export default class extends Component {
                 title: v,
                 // 表头添加字段
                 dataIndex: dataIndex[i],
-                align: 'center'
+                align: 'center',
             })
         })
         // console.log(columns)
@@ -134,23 +154,23 @@ export default class extends Component {
                 )
             }
         })
-        let tableData=[]
+        let tableData = []
         // 表单数据
-        data.map((v,i)=>{
+        data.map((v, i) => {
             tableData.push({
-                deviceId:v.deviceId,
-                deviceTypeName:v.deviceTypeName,
-                name:v.name,
-                installAddr:v.installAddr,
-                IP:`(${v.latitude},${v.longitude})`,
-                enableTime:v.enableTime,
-                opsCompony:v.opsCompony,
-                managerName:v.managerName,
-                gatewayAddr:v.gatewayAddr,
-                factoryNumber:v.factoryNumber,
-                warningRules:v.warningRules,
-                lastRequestTime:v.lastRequestTime,
-                key:i
+                deviceId: v.deviceId,
+                deviceTypeName: v.deviceTypeName,
+                name: v.name,
+                installAddr: v.installAddr,
+                IP: `(${v.latitude},${v.longitude})`,
+                enableTime: v.enableTime,
+                opsCompony: v.opsCompony,
+                managerName: v.managerName,
+                gatewayAddr: v.gatewayAddr,
+                factoryNumber: v.factoryNumber,
+                warningRules: v.warningRules,
+                lastRequestTime: v.lastRequestTime,
+                key: i
             })
         })
         this.setState({
@@ -166,7 +186,7 @@ export default class extends Component {
         })
     }
     // 翻页
-    _pageChange(page){
+    _pageChange(page) {
         console.log(page)
     }
     // 重置搜索表单
@@ -178,30 +198,72 @@ export default class extends Component {
         console.log(456)
     }
     // 导出数据
-    _uploadHandler(){
+    _uploadHandler() {
         console.log('导出数据')
     }
+    //取消显示设置 
+    _setShowCancel() {
+        const form = this.showSetForm.props.form;
+        // 重置表单
+        form.resetFields();
+        this.setState({
+            showSetVisible: false
+        })
+    }
+    // 确定显示设置
+    _setShowOk() {
+        const form = this.showSetForm.props.form;
+        form.validateFields((err, values) => {
+            // values即为表单数据
+            if (err) {
+                return;
+            }
+            let { dataIndex } = values
+            // 过滤后的columns
+            let filterColumns = []
+            // 定义一个title
+            let title = []
+            // 比对dataIndex
+            dataIndex.map((v, i) => {
+                filterColumns.push(...sourceColumns.filter(item => item.dataIndex === v))
+            })
+
+            filterColumns.map((v, i) => {
+                console.log(v.number)
+            })
+            // columns
+            // console.log(this.state.columns)
+            console.log(filterColumns)
+            // this._getTableData(title, this.state.data,dataIndex)
+        })
+        this.setState({
+            showSetVisible: false
+        })
+    }
     render() {
-        const { columns, showSetVisible ,tableData,itemCount} = this.state
+        const { columns, showSetVisible, tableData, itemCount } = this.state
         const paginationProps = {
             showQuickJumper: true,
-            total:itemCount,
+            total: itemCount,
             // 传递页码
             onChange: (page) => this._pageChange(page)
         };
         return (
             <div>
                 <ShowSetForm
-                wrappedComponentRef={(showSetForm)=>this.showSetForm=showSetForm}
-                visible={showSetVisible}
+                    wrappedComponentRef={(showSetForm) => this.showSetForm = showSetForm}
+                    visible={showSetVisible}
+                    onCancel={() => this._setShowCancel()}
+                    onOk={() => this._setShowOk()}
                 />
-                
+
                 <div className={styles.header}>
                     <span>|</span>设备信息
                 </div>
                 <div className={styles.searchGroup}>
                     <SearchForm
                         wrappedComponentRef={(searchForm) => this.searchForm = searchForm}
+
                     />
                     <div className={styles.buttonGroup}
                     >
@@ -231,7 +293,7 @@ export default class extends Component {
                     </Button>
                         <Button
                             icon='upload'
-                            onClick={()=>this._uploadHandler()}
+                            onClick={() => this._uploadHandler()}
                         >
                             导出数据
                     </Button>
@@ -344,17 +406,44 @@ const SearchForm = Form.create()(
     }
 )
 // 显示设置弹窗表单
-const ShowSetForm =Form.create()(
-    class extends React.Component{
-        render(){
-            const {visible,form}=this.props
+const ShowSetForm = Form.create()(
+    class extends React.Component {
+        render() {
+            const { visible, form, onOk, onCancel } = this.props
             const { getFieldDecorator } = form;
             const CheckboxGroup = Checkbox.Group;
             // console.log(totalTitle)
             // console.log(dataIndex)
-            return(
-                <Modal>
-                    
+            return (
+                <Modal
+                    visible={visible}
+                    title="显示设置"
+                    cancelText='取消'
+                    okText='确定'
+                    onOk={onOk}
+                    onCancel={onCancel}
+                >
+                    <Form>
+                        <Form.Item>
+                            {getFieldDecorator('dataIndex', {
+                                initialValue: dataIndex
+                            })
+                                (
+                                <CheckboxGroup>
+                                    <Row>
+                                        {totalTitle.map((v, i) => {
+                                            return (
+                                                <Col key={i} span={8}>
+                                                    <Checkbox value={dataIndex[i]}>{v}</Checkbox>
+                                                </Col>
+                                            )
+                                        })}
+                                    </Row>
+                                </CheckboxGroup>
+                                )
+                            }
+                        </Form.Item>
+                    </Form>
                 </Modal>
             )
         }
