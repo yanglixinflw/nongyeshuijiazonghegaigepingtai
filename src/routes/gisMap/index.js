4611c49ce4cc14043dbf4c63f4a220ad16de1140
zxj462602 <z14758078652@163.com>
2018-10-30 10:41:31 +0800

    gismap

--- src/routes/gisMap/index.js
+++ src/routes/gisMap/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import BreadcrumbView from 'components/PageHeader/breadcrumb';
 import MyMap from '../../components/GisMap/index';
@@ -29,32 +28,3 @@ IndexPage.propTypes = {
 };
 
 export default IndexPage;
-=======
-import React from 'react';
-import BreadcrumbView from 'components/PageHeader/breadcrumb';
-import MyMap from '../../components/GisMap/index';
-class IndexPage extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    return (
-      <div>
-        <div>
-          <BreadcrumbView
-            {...this.props}
-          />
-        </div>
-          
-        <MyMap />
-      </div>
-
-    );
-  }
-}
-
-IndexPage.propTypes = {
-};
-
-export default IndexPage;
->>>>>>> 5d74c4339228794753f2855786ac1162afc311c9
