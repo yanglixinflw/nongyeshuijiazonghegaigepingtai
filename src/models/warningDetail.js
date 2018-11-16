c671d3ce4a1d9941e8fb6e250c7b3fcc55628f8d
zxj462602 <z14758078652@163.com>
2018-11-16 17:29:28 +0800

    更新代码

--- src/models/warningDetail.js
+++ src/models/warningDetail.js
@@ -1,4 +1,4 @@
-import { queryWarningDetail } from '../services/api';
+import { queryWarningDetail,getUserList } from '../services/api';
 export default {
     namespace: 'warningDetail',
     state:{},
@@ -8,10 +8,22 @@ export default {
           yield put({ type: 'fetchOk', payload: data })
         //   console.log('connect成功')
         },
+        // 获取通知人列表
+        *getUserList({payload},{call,put}){
+          const list =yield call(getUserList,payload)
+          yield put ({
+              type:'fetchUserlList',
+              payload:list
+          })
+        }
       },
       reducers: {
         fetchOk (state, { payload }) {
           return { ...state, ...payload }
         },
+        fetchUserlList(state,{payload}){
+          state.userList = payload
+          return {...state}
+        }
       }
 }
