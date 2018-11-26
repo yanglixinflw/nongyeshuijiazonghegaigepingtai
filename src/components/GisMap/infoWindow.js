b04028ad3735498586547b8b74f95b45e88f0344
zxj462602 <z14758078652@163.com>
2018-11-26 14:14:14 +0800

    Gis实时数据显示

--- src/components/GisMap/infoWindow.js
+++ src/components/GisMap/infoWindow.js
@@ -6,16 +6,18 @@ import classnames from 'classnames'
 export default class extends Component {
     constructor(props) {
         super(props)
-        console.log(props)
+        // console.log(props)
     }
     render() {
         const {info} = this.props
+        if(info.length == 0){
+            return null
+        }
         const {infoData} =this.props
-        const { isWarning,deviceTypeId } = info[0]
-        if (deviceTypeId == 5) {
+        if (info[0].deviceTypeId == 5) {   //摄像头
             return (
                 <div>
-                    {isWarning ?
+                    {info[0].isWarning ?
                         <div className={styles.cameraWarningWindow}>
                             <p className={styles.warinngMsg}>设备网络断开，请重新连接网络</p>
                             <div className={styles.mask}>{info[0].name}</div>
@@ -28,13 +30,13 @@ export default class extends Component {
                     }
                 </div>
             )
-        } else if (deviceTypeId == 1) {
+        } else if (info[0].deviceTypeId == 1) {  //球阀
             return (
                 <div>
                     <div className={styles.infoTitle}>
                         <p className={styles.deviceName}>{info[0].name}</p>
                     </div>
-                    {isWarning ?
+                    {info[0].isWarning ?
                         <div className={styles.infoContent}>
                             <p className={styles.warinngMsg}>设备网络断开，请重新连接网络</p>
                             <p className={styles.checkMore}><a>点击查看更多信息<span>>></span></a></p>
@@ -46,19 +48,19 @@ export default class extends Component {
                                     <p>暂无数据</p>
                                 :
                                 infoData.map((v,i)=>{
-                                    return (
-                                        <div>
-                                            <p className={styles.labelName} key={i}>太阳能电压<span>{v.sunElecPress}</span></p>
-                                            <p className={styles.labelName} key={i}>管道压力<span>{v.press}</span></p>
-                                            <p className={styles.labelName} key={i}>瞬时流量<span>{v.instantNumber}</span></p>
-                                            <p className={styles.labelName} key={i}>累积流量<span>{v.waterTotalNumber}</span></p>
-                                            <p className={styles.labelName} key={i}>供电电压<span>{v.batteryPress}</span></p>
-                                            <p className={styles.labelName} key={i}>阀门状态<span>{v.valveStatus}</span></p>
-                                        </div>
-                                    )
+                                    let result =[]
+                                    for(let key in v){
+                                        result.push(
+                                            <div>
+                                                <p className={styles.labelName} key={key}>{key}<span>{v[key]}</span></p>
+                                            </div>
+                                        )
+                                            
+                                        
+                                    }
+                                   return result
                                 })
                             }
-
                             <p className={styles.labelName}>
                                 控制开关
                             <Switch defaultChecked />
@@ -68,24 +70,83 @@ export default class extends Component {
                     }
                 </div>
             )
-        } else {
+        } else if (info[0].deviceTypeId == 2){ //水表
             return (
                 <div>
                     <div className={styles.infoTitle}>
                         <p className={styles.deviceName}>{info[0].name}</p>
                     </div>
-                    {isWarning ?
+                    {info[0].isWarning ?
                         <div className={styles.infoContent}>
                             <p className={styles.warinngMsg}>设备网络断开，请重新连接网络</p>
                             <p className={styles.checkMore}><a>点击查看更多信息<span>>></span></a></p>
                         </div>
                         :
                         <div className={styles.infoContent}>
-                            <p className={styles.labelName}>实时数据<span>0.99m3/h</span></p>
-                            <p className={styles.labelName}>
+                            {
+                                infoData.length == 0? 
+                                    <p>暂无数据</p>
+                                :
+                                infoData.map((v,i)=>{
+                                    let result =[]
+                                    for(let key in v){
+                                        result.push(
+                                            <div>
+                                                <p className={styles.labelName} key={key}>{key}<span>{v[key]}</span></p>
+                                            </div>
+                                        )
+                                            
+                                        
+                                    }
+                                   return result
+                                })
+
+                            }
+                            {/* <p className={styles.labelName}>
                                 控制开关
                             <Switch defaultChecked />
-                            </p>
+                            </p> */}
+                            <p className={styles.checkMore}><a>点击查看更多信息<span>>></span></a></p>
+                        </div>
+                    }
+                </div>
+            )
+        }else if (info[0].deviceTypeId == 3){ //电表
+            return (
+                <div>
+                    <div className={styles.infoTitle}>
+                        <p className={styles.deviceName}>{info[0].name}</p>
+                    </div>
+                    {info[0].isWarning ?
+                        <div className={styles.infoContent}>
+                            <p className={styles.warinngMsg}>设备网络断开，请重新连接网络</p>
+                            <p className={styles.checkMore}><a>点击查看更多信息<span>>></span></a></p>
+                        </div>
+                        :
+                        <div className={styles.infoContent}>
+                           {
+                                infoData.length == 0? 
+                                    <p>暂无数据</p>
+                                :
+                                infoData.map((v,i)=>{
+                                    let result =[]
+                                    for(let key in v){
+                                        result.push(
+                                            <div>
+                                                <p className={styles.labelName} key={key}>{key}<span>{v[key]}</span></p>
+                                            </div>
+                                        )
+                                            
+                                        
+                                    }
+                                   return result
+                                })
+
+                            }
+                            {/* <p className={styles.labelName}>
+                                控制开关
+                            <Switch defaultChecked />
+                            </p> */}
                             <p className={styles.checkMore}><a>点击查看更多信息<span>>></span></a></p>
                         </div>
                     }
