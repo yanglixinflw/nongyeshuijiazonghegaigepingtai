552042a27f1c256fc384d64d8f7e8bc3d2d06f43
cph-code <cheng19970210@qq.com>
2018-12-08 15:02:56 +0800

    农户信息页面改正完成

--- src/components/infoManagement/farmersInfo.js
+++ src/components/infoManagement/farmersInfo.js
@@ -887,7 +887,7 @@ const EditPwdForm = Form.create()(
           compareToFirstPassword = (rule, value, callback) => {
             const form = this.props.form;
             if (value && value !== form.getFieldValue('password')) {
-              callback('Two passwords that you enter is inconsistent!');
+              callback('两次输入密码不一致!');
             } else {
               callback();
             }
